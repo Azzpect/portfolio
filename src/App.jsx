eda6aad7719f8bfcc2f580ce882dc11c129ad8a8
Azzpect <atanughosh.brp@gmail.com>
2024-07-27 00:45:01 +0530

    structured home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./css/output.css"
+import github from "./assets/github.svg"
+import linkedin from "./assets/linkedin.svg"
+import twitter from "./assets/twitter.svg"
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="w-screen h-[8vh] flex justify-around">
+        <img src='./src/assets/logo.png' className=""/>
+        <nav className="w-[40vw] mr-[15%] flex justify-around items-center">
+          <a href='' className='navitem'>Home</a>
+          <a href='' className='navitem'>Work</a>
+          <a href='' className='navitem'>Contact</a>
+        </nav>
+      </header>
+      <section className="handles-container">
+        <a><img src={github} className="handle"></img></a>
+        <a><img src={linkedin} className="handle"></img></a>
+        <a><img src={twitter} className="handle"></img></a>
+      </section>
+      <section className="intro">
+        <div className="flex flex-col justify-around items-center">
+          <img src="./src/assets/myimage.jpg" className="rounded-[50%]"/>
+          <p className="font-sig text-2xl leading-[5rem]">Hello, I am Atanu Ghosh.</p>
+        </div>
+        <div className="w-[40%] h-[60%] relative">
+          <h1 className="font-heading text-[2rem] leading-10 font-semibold">I am a full stack web developer.</h1>
+          <h5 className="font-heading text-xs leading-10">passionate about problem solving and creating robust applications</h5>
+          <div className="btn-container absolute bottom-12">
+            <button className="btn bg-[#94DEF5]">See my work</button>
+            <button className="btn bg-[#D9D9D9]">Read more about me</button>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,css}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      navitem: ["Julius Sans One"],
+      heading: ["Vollkorn SC"],
+      sig: ["Lavishly Yours"],
+    },
+  },
+  plugins: [],
+}
+
